977c7013bb72fa46319971c70fa10c8b0c466a89
Jo Hill <jos.a.hill@gmail.com>
2022-05-01 15:51:29 -0700

    fixed user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,9 +7,6 @@ const userController = {
   getAllUser(req, res) {
       User.find({})
           .select('-__v')
-          .sort({
-              _id: -1
-          })
           .then(dbUserData => res.json(dbUserData))
           .catch(err => {
               console.log(err);
